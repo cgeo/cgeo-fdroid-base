b5a44a9180b57b3bbad4731a85748a7f69ecb744
Mathieu <github@kumy.net>
2019-04-29 22:20:30 +0200

    Allow to read credentials from env or file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-slave=$(cat /srv/slave)
-secret=$(cat /srv/secret)
-if [ -z "$slave" -o -z "$secret" ]; then
-  echo "You must place credentials for Jenkins in /srv/slave and /srv/secret" 2> /dev/null
+if [ -z "$JENKINS_NODE_NAME" ]; then
+  JENKINS_NODE_NAME_FILE=${JENKINS_NODE_NAME_FILE:-/srv/slave}
+  JENKINS_NODE_NAME=$(cat $JENKINS_NODE_NAME_FILE)
+fi
+if [ -z "$JENKINS_NODE_SECRET" ]; then
+  JENKINS_NODE_SECRET_FILE=${JENKINS_NODE_SECRET_FILE:-/srv/secret}
+  JENKINS_NODE_SECRET=$(cat $JENKINS_NODE_SECRET_FILE)
+fi
+
+if [ -z "$JENKINS_NODE_NAME" -o -z "$secret" ]; then
+  echo "No credentials detected. Please check README for the ways to provide them." 2> /dev/null
   exit 1
 fi
 
-java -jar /apk/slave.jar -jnlpUrl http://ci.cgeo.org/computer/$slave/slave-agent.jnlp -secret $secret
+java -jar /apk/slave.jar -jnlpUrl http://ci.cgeo.org/computer/$JENKINS_NODE_NAME/slave-agent.jnlp -secret $JENKINS_NODE_SECRET
